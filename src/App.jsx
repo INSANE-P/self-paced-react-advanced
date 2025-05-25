9905d9e90adfd1d4a2e91d0f7396391532440193
INSANE-P <chanbin0626@gmail.com>
2025-05-25 22:27:58 +0900

    refactor: Provider들을 AppProvider로 통합(가독성)

--- src/App.jsx
+++ src/App.jsx
@@ -3,30 +3,21 @@ import CategoryFilter from './components/main/CategoryFilter';
 import RestaurantList from './components/main/RestaurantList';
 import RestaurantDetailModal from './components/aside/RestaurantDetailModal';
 import AddRestaurantModal from './components/aside/AddRestaurantModal';
-import { ThemeProvider } from 'styled-components';
-import { theme } from './styles/theme';
-import GlobalStyles from './styles/GlobalStyles';
-import { RestaurantProvider } from './contexts/RestaurantContext';
-import { ModalProvider } from './contexts/ModalContext';
+import AppProvider from './contexts/AppProvider';
 
 const App = () => {
   return (
-    <RestaurantProvider>
-      <ModalProvider>
-        <ThemeProvider theme={theme}>
-          <GlobalStyles />
-          <Header />
-          <main>
-            <CategoryFilter />
-            <RestaurantList />
-          </main>
-          <aside>
-            <RestaurantDetailModal />
-            <AddRestaurantModal />
-          </aside>
-        </ThemeProvider>
-      </ModalProvider>
-    </RestaurantProvider>
+    <AppProvider>
+      <Header />
+      <main>
+        <CategoryFilter />
+        <RestaurantList />
+      </main>
+      <aside>
+        <RestaurantDetailModal />
+        <AddRestaurantModal />
+      </aside>
+    </AppProvider>
   );
 };
 
